3305d2c0e4d1ffd5af13581d1a64c4f45af54c8f
AliBakhtiari <arshiapanahianzzna6@gmail.com>
2025-07-23 14:07:55 +0330

    fix: Complete initial project setup and resolve config errors

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{ fontFamily: 'system-ui', textAlign: 'center', padding: '50px' }}>
+      <h1>Hello to SE Lab</h1>
+    </div>
+  );
+}
+
+export default App;
